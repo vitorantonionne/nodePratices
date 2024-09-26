38045c141abb06c73f4e332d297f7359505eb1cb
vitorantonionne <vitor-antonionne@hotmail.com>
2024-09-26 19:36:22 -0300

    in progress

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import { Database } from "./database.js"
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js"
+
+const database = new Database()
+
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler:(req, res) => {
+      const users = database.select('users')
+
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { name, email } = req.body
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email
+      }
+
+      database.insert('users', user)
+
+      return res.writeHeader(201).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params.id
+      
+      database.delete('users', id)
+
+      return res.writeHeader(204).end()
+    }
+  }
+]
