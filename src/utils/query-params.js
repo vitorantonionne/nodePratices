e202688d192297514115e82006c76d4d4249bbe8
vitorantonionne <vitor-antonionne@hotmail.com>
2024-09-30 19:35:40 -0300

    regex url

---
+++ src/utils/query-params.js
@@ -0,0 +1,11 @@
+
+
+export function extractionQueryParams(query) {
+  return query.substr(1).split('&').reduce((queryParams, param) => {
+    const [key, value] = param.split('=')
+
+    queryParams[key] = value
+2
+    return queryParams
+  }, {})
+}
